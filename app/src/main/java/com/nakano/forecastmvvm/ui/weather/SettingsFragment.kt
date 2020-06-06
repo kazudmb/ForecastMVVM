767b0242bb8f85dc4c15d5e4428c02b20e6e8e04
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-06-06 17:37:44 +0900

    Initial commit

---
+++ app/src/main/java/com/nakano/forecastmvvm/ui/weather/SettingsFragment.kt
@@ -0,0 +1,8 @@
+package com.nakano.forecastmvvm.ui.weather
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class SettingsFragment: Fragment() {
+
+}
---
+++ app/src/main/java/com/nakano/forecastmvvm/ui/weather/future/detail/FutureDetailWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.nakano.forecastmvvm.ui.weather.future.detail
+
+import androidx.lifecycle.ViewModel
+
+class FutureDetailWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
